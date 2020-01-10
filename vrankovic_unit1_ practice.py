9d0c55da512eab9087a0b0c90c22aa8939062a4f
23vrange <59451426+23vrange@users.noreply.github.com>
2020-01-10 09:39:27 -0500

    Add files via upload

---
+++ vrankovic_unit1_ practice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# PART A
+print("Welcome to Information Technology\n\nProgramming and\nWeb Development")
+print("")
+print(" _       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/" 
+" / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+#PART B
+print("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
+
+print("    _____________________________________\n   /--------------------------/|_________|\n  /--------------------------/ |_________|\n /--------------------------/  |_________|\n/--------------------------/   |_________|\n|______________________________|_________|\n|    _______        _______    |_________|\n|    |   ]  |       |  ]   |   |_________|\n|    |======|       |======|   |_________|\n|    |___]__|       |__]___|   |_________|\n|            _______           |_________|\n|           |       |          |_________|\n|           |      @|          |_________|\n|           |       |          |_________|")
+print("name georgije     favorite animal squid      favorite number 5")
+print("________________\n|              |\n|              |\n|              |\n|              |  10\n|              |\n|______________|\n        10")
+print("Georgije Vrankovic")
+print("")
+print("w = wake  m = me  u = up  i = inside")
+print("")
+print("w+m+p w+m+u+i cant+w+u w+m+u+i+save+m")
+print("Enter a name: bob\n Enter an adjective: sticky\n Enter an adjective: brown\n Enter an adverb: slow \n Enter a food: meat ball\n Enter another food: chicken\n Enter a noun: poop\n Enter a place: disappointment island\n Enter a verb: poop")
+print("bob was planning a dream vacation to disappointment island. bob was especially looking forward to trying the \nlocal cuisine, including sticky chicken and meat balls. bob will have to practice the language slowly to make \nit easier to poop with people. bob has a long list of sights to see, including the poop museum and the \nbrown park")
